f009ab3d7f429672bfa0399d38ae55500f959fe4
sakshi <psakshi989@gmail.com>
2026-06-12 00:19:43 +0530

    Add dynamic port configuration for deployment

--- app.js
+++ app.js
@@ -139,7 +139,8 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log("Server started on port " + PORT);
 });
-// C57tnGrqfUV5SytZ
